37ed3c721681f9f9dff1e8262ed4a7e3374b3961
Samuel Fortunato <samfortunato@gmail.com>
2023-06-02 03:44:13 +0900

    Start adding player jumping ability

--- entities/player/player.js
+++ entities/player/player.js
@@ -1,4 +1,6 @@
 import { isKeyPressed } from '../../engine/input.js';
+import { setPlayerPos } from '../../engine/meta.js';
+import { GRAVITY } from '../../engine/physics.js';
 
 import { Entity } from '../entity.js';
 
@@ -10,6 +12,7 @@ import { CROUCH_SPEED, NORMAL_SPEED } from './constants.js';
 export class Player extends Entity {
   state = STATES.IDLE;
   speed = NORMAL_SPEED;
+  zVelocity = 0;
 
   constructor(x = 0, y = 0) {
     super();
@@ -19,6 +22,8 @@ export class Player extends Entity {
   }
 
   update(dt) {
+    if (this.transform.z === 0) this.state = STATES.IDLE;
+
     if (isKeyPressed('Shift')) {
       this.state = STATES.CROUCHING;
       this.speed = CROUCH_SPEED;
@@ -27,19 +32,36 @@ export class Player extends Entity {
       this.speed = NORMAL_SPEED;
     }
 
+    if (isKeyPressed(' ')) {
+      this.state = STATES.JUMPING;
+      this.transform.z += 3;
+    }
+
+    this.transform.z -= GRAVITY;
+    if (this.transform.z < 0) this.transform.z = 0;
+
     if (isKeyPressed('ArrowUp')) this.transform.y -= clampToPixel(this.speed * dt);
     if (isKeyPressed('ArrowRight')) this.transform.x += clampToPixel(this.speed * dt);
     if (isKeyPressed('ArrowDown')) this.transform.y += clampToPixel(this.speed * dt);
     if (isKeyPressed('ArrowLeft')) this.transform.x -= clampToPixel(this.speed * dt);
+
+    setPlayerPos(this.transform);
   }
 
+  /** @param {CanvasRenderingContext2D} ctx */
   draw(ctx) {
     ctx.fillStyle = '#000000';
 
     if (this.state === STATES.CROUCHING) {
-      ctx.fillRect(this.transform.x, this.transform.y + 12, 32, 20);
+      ctx.fillRect(this.transform.x, (this.transform.y - this.transform.z) + 12, 32, 20);
     } else {
-      ctx.fillRect(this.transform.x, this.transform.y, 32, 32);
+      ctx.fillRect(this.transform.x, this.transform.y - this.transform.z, 32, 32);
     }
+
+    // shadow
+    ctx.globalAlpha = 0.5;
+    ctx.fillStyle = 'black';
+    ctx.fillRect(this.transform.x, this.transform.y + 12, 32, 20);
+    ctx.globalAlpha = 1;
   }
 }
--- scenes/start.js
+++ scenes/start.js
@@ -10,13 +10,15 @@ import { Scrollable } from '../entities/scrollable.js';
 import { TextWindow } from '../entities/text-window/text-window.js';
 import { Clickable } from '../entities/clickable.js';
 import { AreaInfo } from '../entities/area-info.js';
+import { EventTrigger } from '../entities/event.js';
 
 export class StartScene extends Scene {
   initialize() {
     addEntities([
-      new Player(400, 400),
       new Scrollable(),
       new Clickable(500, 500),
+      new EventTrigger(300, 300),
+      new Player(400, 400),
       new TextWindow('hello, this is a text box.', 20, 20, 300, 200),
       new ChatWindow(),
       new AreaInfo(),
