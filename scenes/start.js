077645135d713756b28a578db0275b8c00790135
Samuel Fortunato <samfortunato@gmail.com>
2023-06-06 19:43:28 +0900

    Modify text box position code for start scene

--- scenes/start.js
+++ scenes/start.js
@@ -5,7 +5,7 @@ import { Scene } from './scene.js';
 import { DebugInfo } from '../entities/debug-info.js';
 
 import { Player } from '../entities/player/player.js';
-import { ChatWindow } from '../entities/chat-window.js';
+import { ChatWindow } from '../entities/chat-window/chat-window.js';
 import { Scrollable } from '../entities/scrollable.js';
 import { TextWindow } from '../entities/text-window/text-window.js';
 import { Clickable } from '../entities/clickable.js';
@@ -19,7 +19,7 @@ export class StartScene extends Scene {
       new Clickable(500, 500),
       new EventTrigger(300, 300),
       new Player(400, 400),
-      new TextWindow('hello, this is a text box.', 20, 20, 300, 200),
+      new TextWindow('hello, this is a text box.', 0, 0, 300, 200),
       new ChatWindow(),
       new AreaInfo(),
       new DebugInfo(16, 16),
