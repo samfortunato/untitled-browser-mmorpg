ecf55a9d0c182c38e1bc8fab4d83a9720d757287
Samuel Fortunato <samfortunato@gmail.com>
2023-06-06 19:38:37 +0900

    Show FPS when debug info is on screen

--- entities/debug-info.js
+++ entities/debug-info.js
@@ -1,15 +1,20 @@
+import { getFps } from '../engine/time.js';
+
 import { Entity } from './entity.js';
 
 export class DebugInfo extends Entity {
   dt = 0;
+  fps = 0;
 
   update(dt) {
     this.dt = dt;
+    this.fps = getFps();
   }
 
   draw(ctx) {
     ctx.fillStyle = 'white';
     ctx.font = '1rem serif';
     ctx.fillText(this.dt, this.transform.x, this.transform.y);
+    ctx.fillText(`${this.fps} FPS`, this.transform.x, this.transform.y + 20);
   }
 }
