5f5cbe60e434c10e41d12f1beff1ab9be806b78b
Samuel Fortunato <samfortunato@gmail.com>
2023-06-19 13:04:52 +0900

    Add background music to start map

--- scenes/scene.js
+++ scenes/scene.js
@@ -2,16 +2,23 @@ import { allEntities } from '../engine/entity.js';
 
 import { GameMap } from '../maps/game-map.js';
 
+import { MusicEmitter } from '../components/music-emitter.js';
+
 export class Scene {
   map = GameMap;
+  music = new MusicEmitter('somber');
 
   getMap() {
     return this.map;
   }
 
-  initialize() { }
+  initialize() {
+    this.music.loop();
+  }
 
   update(dt) {
+    this.music.update();
+
     for (const entity of allEntities.values()) {
       entity.update(dt);
     }
--- scenes/start.js
+++ scenes/start.js
@@ -2,10 +2,9 @@ import { addEntities } from '../engine/entity.js';
 
 import { Scene } from './scene.js';
 
-import { DebugInfo } from '../entities/debug-info.js';
-
 import { StartMap } from '../maps/start.js';
 
+import { DebugInfo } from '../entities/debug-info.js';
 import { Player } from '../entities/player/player.js';
 import { ChatWindow } from '../entities/chat-window/chat-window.js';
 import { Scrollable } from '../entities/scrollable.js';
@@ -16,20 +15,27 @@ import { EventTrigger } from '../entities/event.js';
 import { Truck } from '../entities/truck.js';
 import { BlueGuy } from '../entities/blue-guy/blue-guy.js';
 import { Bee } from '../entities/bee/bee.js';
+import { Menu } from '../entities/menu.js';
+
+import { MusicEmitter } from '../components/music-emitter.js';
 
 export class StartScene extends Scene {
   map = StartMap;
+  music = new MusicEmitter('null');
 
   initialize() {
+    super.initialize();
+
     addEntities([
-      new Scrollable(),
-      new Clickable(500, 500),
+      // new Scrollable(),
+      // new Clickable(500, 500),
       new EventTrigger(300, 300),
       new Truck(),
       new BlueGuy(),
       new Player(400, 400),
       new Bee(400, 450),
-      new TextWindow('hello, this is a text box.', 0, 0, 300, 200),
+      new Menu(),
+      // new TextWindow('hello, this is a text box.', 0, 0, 300, 200),
       new ChatWindow(),
       new AreaInfo(),
       new DebugInfo(16, 16),
