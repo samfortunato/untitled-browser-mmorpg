6c0023674cad2769ed79e6accdeb62cd18daa0f7
Samuel Fortunato <samfortunato@gmail.com>
2023-06-21 23:29:57 +0900

    Add map editor command and temporary log map command (for saving maps)

---
+++ engine/command.js
@@ -0,0 +1,25 @@
+import { addEntity } from './entity.js';
+
+import { MapEditor } from '../entities/map-editor/map-editor.js';
+
+import { getCurrentSceneMap } from './scene.js';
+
+const COMMANDS = {
+  'mapeditor': onMapEditor,
+  'logmap': onLogMap,
+}
+
+function onMapEditor() {
+  addEntity(new MapEditor());
+}
+
+function onLogMap() {
+  console.log(getCurrentSceneMap().tiles);
+}
+
+export function processCommand(commandText) {
+  const commandName = commandText.slice(1);
+  const command = COMMANDS[commandName];
+
+  if (command) command();
+}
