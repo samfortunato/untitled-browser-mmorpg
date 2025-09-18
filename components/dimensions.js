eb4254569c626fec5a2030adcb34917c0aa3f799
Samuel Fortunato <samfortunato@gmail.com>
2025-09-17 19:10:15 -0700

    Add note to change name of `Dimensions` component to something nicer, like "Shape"?
    Also, make common math properties short names?

--- components/dimensions.js
+++ components/dimensions.js
@@ -1,7 +1,21 @@
+/**
+ * TODO: maybe change to `Shape`, or something?
+ *
+ * also, make `width` and `height` just `w` and `h`?
+ */
+
 /** Represents 2D dimensions of an object in space. */
 export class Dimensions {
   constructor(width = 0, height = 0) {
     this.width = width;
     this.height = height;
+  }
+}
+
+/** Represents 2D dimensions of an object in space. */
+export class Shape {
+  constructor(w = 0, h = 0) {
+    this.w = w;
+    this.h = h;
   }
 }
